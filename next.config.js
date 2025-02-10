10974488e9bd1b8d999a4265b36093ed96ee0fda
furukawa1020 <f.kotaro.0530@gmail.com>
2025-02-10 21:50:13 +0900

    komitti

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/your-repository-name',
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+
+  
